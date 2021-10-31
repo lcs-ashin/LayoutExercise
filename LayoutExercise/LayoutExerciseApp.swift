7fa2eb02069d4056c88298b74874269da20eb086
lcs-ashin <ashin@lcs.on.ca>
2021-10-31 11:41:20 -0400

    Initial Commit

---
+++ LayoutExercise/LayoutExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExerciseApp.swift
+//  LayoutExercise
+//
+//  Created by Eunbi Shin on 2021-10-31.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
